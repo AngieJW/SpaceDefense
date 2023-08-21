cd81fef82f46d51565b4a399b01351b3d00793a9
Angie Duhard <angie.duhard@gmail.com>
2023-08-21 23:02:44 +0200

    grid for fleet

---
+++ ANSWERS.md
@@ -0,0 +1,4 @@
+1. What is the complexity of your algorithm (in big O notation)?
+2. How would you improve your algorithm?
+3. How would your adapt your algorithm to three dimensions? How would that affect
+the complexity?
--- space_defense.py
+++ space_defense.py
@@ -1,3 +1,5 @@
+import random
+
 # You are the admiral of a mighty space fleet of 50 vessels. Your fleet consists of two major
 # types of vessels - support craft and offensive craft. Vessels can all receive a command
 # that tells them to move to given coordinates.
@@ -31,17 +33,34 @@ class Vessel:
 class Support(Vessel):
     def __init__(self, vessel_type, task):
         super().__init__(vessel_type)
+
+        allowed_tasks = ["refueling", "assistance", "cargo"]
+        if task not in allowed_tasks:
+            raise ValueError("Task must be one of {}".format(allowed_tasks))
+
         self.medical_unit = True
-        self.task = task  # refueling, mechanical assistance, cargo
+        self.task = task
 
     def __str__(self):
         return self.task
 
 class Offensive(Vessel):
+    allowed_vessel_types = ["battleship", "destroyer", "cruiser"]
+    cannon_numbers = {
+        "battleship": 24,
+        "destroyer": 12,
+        "cruiser": 6
+    }
+
     def __init__(self, vessel_type, cannons):
         super().__init__(vessel_type)
-        self.cannons = cannons  # battleships have 24, destroyers have 12, cruisers have 6
+        # self.cannons = cannons  # battleships have 24, destroyers have 12, cruisers have 6
         self.shield = False
+
+        if vessel_type not in self.allowed_vessel_types:
+            raise ValueError("Vessel type must be one of {}".format(self.allowed_vessel_types))
+
+        self.cannons = self.cannon_numbers[vessel_type]
 
     # command to attack
     def attack(self):
@@ -52,7 +71,65 @@ class Offensive(Vessel):
     def raise_shield(self):
         self.shield = True
 
+# create a fleet of 50 vessels
 fleet = []
 command_ship = Offensive("CommandShip", cannons=24)
 fleet.append(command_ship)
 print(fleet)
+
+for _ in range(25):
+    random_task = random.choice(allowed_tasks)
+    support_vessel = Support("support", random_task)
+    fleet.append(support_vessel)
+
+for _ in range(24):
+    offensive_vessel = Offensive("offensive", cannons=24)
+    fleet.append(offensive_vessel)
+
+
+# You are taking your fleet, made up of an equal number of offensive and support ships, to
+# your assigned deployment point when you are ambushed by enemy forces. Your defense
+# tactic is to pair each support ship with one offensive ship in order to share the offensive
+# ship's shield.
+# Assuming a two-dimensional layout with a maximum size of 100x100, write some code that
+# is able to represent your fleet location data and populate it with your 50 ships in random
+# positions. Then, implement an algorithm that generates 25 pairs of ships, and issues the
+# commands to make the pairs occupy adjacent positions on the grid by moving one or both
+# ships. Your vessels need to assume this defensive formation as quickly as possible, so you
+# will need to find an algorithm that gives an optimized set of pairs, but that is also quick to
+# generate them
+
+
+# Define the class for the grid
+class Grid:
+    def __init__(self, size):
+        self.size = size
+        self.grid = [[0 for x in range(size)] for y in range(size)]
+
+    def place_ship(self, ship, x, y):
+        self.grid[x][y] = ship
+        ship.move((x, y))
+
+# Initialize the grid
+# Place the ships on the grid at random positions
+# Generate pairs of ships for formation (one support + one offensive)
+# Iterate through the pairs and have their positions to be in adjacent cells
+# Update the grid with new positions
+
+grid = Grid(size=100)
+
+for ship in fleet:
+    while True:
+        x = random.randint(0, 99)
+        y = random.randint(0, 99)
+        if grid.grid[x][y] == 0:
+            grid.place_ship(ship, x, y)
+            break
+
+# Generate pairs of ships for formation (one support one offensive).
+pairs = []
+offensive_ships = [ship for ship in fleet if isinstance(ship, Offensive)]
+support_ships = [ship for ship in fleet if isinstance(ship, Support)]
+
+for offensive, support in zip(offensive_ships, support_ships):
+    pairs.append((offensive, support))
